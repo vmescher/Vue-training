ac8ba9a9a4871f9b94147c5f0285937d18a0b0ea
vmescher <mescher174@gmail.com>
2021-06-09 10:46:16 +0500

    test

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="block">
+    <form action="#" method="get" class="block__form form">
+      <input
+        class="form__input"
+        v-model="message"
+        type="text"
+        placeholder="Введите текст"
+      />
+      <input
+        type="submit"
+        class="form__button"
+        value="Подтвердить"
+        @click="getName()"
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: null,
+    };
+  },
+  methods: {
+    getName() {
+      console.log(this.message);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.block {
+  max-width: 350px;
+  margin: 0 auto;
+}
+</style>
